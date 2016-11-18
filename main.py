ab2931562626af0541d58cb50b458a33bc3973b7
Kurt Ruegg <kruegg@Kurts-MacBook-Pro.local>
2016-11-17 19:03:48 -0800

    auto push

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ def graphlab_recommender_test():
                                                train = False)
 
     # Make predictions
-    album_list = ['https://toucheamore.bandcamp.com/album/is-survived-by']
+    album_list = ['https://openmikeeagle360.bandcamp.com/album/dark-comedy']
     rating_list = [1] * len(album_list)
     _id_list = ['https://bandcamp.com/kruegg'] * len(album_list)
 
