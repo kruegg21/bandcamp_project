c7c4ee0af6b33e610c163abaa46eab82db67fea6
Kurt Ruegg <kruegg@Kurts-MacBook-Pro.local>
2016-11-18 14:56:38 -0800

    auto push

--- model.py
+++ model.py
@@ -65,7 +65,7 @@ def graphlab_grid_search(sf, specs = None):
                                       graphlab.ranking_factorization_recommender.create,
                                       specs.param_grid,
                                       evaluator = custom_evaluation)
-    print jobs.get_metrics()
+    print job.get_metrics()
     print job.get_results()
 
     # Put optimal parameters in specifications
