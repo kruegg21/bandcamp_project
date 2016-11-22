65cefd3128b76505adb7992d236c889021afbc33
Kurt Ruegg <kruegg@Kurts-MacBook-Pro.local>
2016-11-21 18:34:59 -0800

    auto push

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def dashboard():
 @app.route('/results', methods=['POST', 'GET'])
 def results():
     data = request.form['exampleTextarea']
-    return 'hello'
+    return data
 
 if __name__ == '__main__':
   app.run(host = "0.0.0.0", port = int("8000"), debug = True)
