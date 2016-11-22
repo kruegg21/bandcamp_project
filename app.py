b2f75cf6266d359faa6f710a03c53f35f1920cf3
Kurt Ruegg <kruegg@Kurts-MacBook-Pro.local>
2016-11-21 18:13:25 -0800

    auto push

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def results():
     return data
 
 if __name__ == '__main__':
-  app.run(host = "0.0.0.0", port = int("8000"), debug = True))
+  app.run(host = "0.0.0.0", port = int("8000"), debug = True)
