69c91fe6a4aee10b7314a80bf2e4b6a7ed80aa9b
Kurt Ruegg <kruegg@Kurts-MacBook-Pro.local>
2016-11-23 14:17:47 -0800

    auto push

--- serendipity_model.py
+++ serendipity_model.py
@@ -14,7 +14,7 @@ def build_serendipity_model(data = None, should_grid_search = True,
     sf = graphlab.SFrame.read_csv('data/{}.csv'.format(data))
 
     # Add serendipity
-    sf = add_serendipity(sf)
+    sf = add_serendipity(sf, specs = specs)
     print sf
 
     # Train
@@ -27,16 +27,55 @@ def build_serendipity_model(data = None, should_grid_search = True,
     if should_make_test_predictions:
         make_test_predictions(model)
 
-def add_serendipity(sf):
+def add_serendipity(sf, specs = None):
     album_url_to_count_dict = get_album_url_to_count_dict()
     sf['album_counts'] = sf.apply(lambda x: album_url_to_count_dict[x['album_id']]).astype(float)
-    print sf
     sf['log_album_counts'] = np.log(sf['album_counts'])
+    sf['serendipity_score'] = sf['log_album_counts'] * specs.serendipity_coefficient
     return sf
+
+def serendipity_recommender(sf, specs, dump = True, train = True,
+                            should_grid_search = False):
+    # Test Train Split
+    if specs.should_shuffle_folds:
+        shuffled_sf = graphlab.toolkits.cross_validation.shuffle(sf)
+        (train_set, test_set) = shuffled_sf.random_split(0.8, seed = 1)
+    else:
+        (train_set, test_set) = sf.random_split(0.8, seed = 1)
+
+    # Grid Search
+    if should_grid_search:
+        grid_search(train_set,
+                    model_factory = graphlab.ranking_factorization_recommender.create,
+                    specs = specs)
+
+    # Create model
+    rec_model = graphlab.ranking_factorization_recommender.create(
+                  train_set,
+                  target = specs.params['target'],
+                  user_id = specs.params['user_id'],
+                  item_id = specs.params['item_id'],
+                  binary_target = specs.params['binary_target'],
+                  max_iterations = specs.params['max_iterations'],
+                  num_factors = specs.params['num_factors'],
+                  ranking_regularization = specs.params['ranking_regularization'],
+                  linear_regularization = specs.params['linear_regularization'],
+                  unobserved_rating_value = specs.params['unobserved_rating_value'],
+                  regularization = specs.params['regularization'])
+
+    # Data print out
+    print rec_model.evaluate_precision_recall(test_set, cutoffs = [100,200,1000], exclude_known = False)
+    print rec_model.get_similar_items()
+
+    # Dump
+    if dump:
+        rec_model.save('factorization_recommender')
+
+    return rec_model
 
 if __name__ == "__main__":
     # Grid Search Parameters
-    param_grid = dict([('target', 'rating'),
+    param_grid = dict([('target', 'serendipity_score'),
                        ('user_id', '_id'),
                        ('item_id', 'album_id'),
                        ('binary_target', False),
@@ -47,7 +86,7 @@ if __name__ == "__main__":
                        ('num_sampled_negative_examples', [4])
                       ])
 
-    params = dict([('target', 'rating'),
+    params = dict([('target', 'serendipity_score'),
                    ('user_id', '_id'),
                    ('item_id', 'album_id'),
                    ('binary_target', False),
@@ -63,6 +102,7 @@ if __name__ == "__main__":
                                  user_count_max_cutoff = np.inf,
                                  album_count_max_cutoff = 1200,
                                  album_count_min_cutoff = 40,
+                                 serendipity_coefficient = 1,
                                  folds = 10,
                                  should_shuffle_folds = True)
 
