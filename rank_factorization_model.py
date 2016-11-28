01ae0e441313b5943f93802e8422434b7c4d6f36
Kurt Ruegg <kruegg@Kurts-MacBook-Pro.local>
2016-11-27 18:04:06 -0800

    auto push

--- rank_factorization_model.py
+++ rank_factorization_model.py
@@ -52,9 +52,14 @@ def rank_factorization_recommender(sf, specs, dump = True, train = True,
                   unobserved_rating_value = specs.params['unobserved_rating_value'],
                   regularization = specs.params['regularization'])
 
-    # Data print out
+    # Display Precision on test set
     print rec_model.evaluate_precision_recall(test_set, cutoffs = [100,200,1000], exclude_known = False)
     print rec_model.get_similar_items()
+
+    # Retrain on full data set
+    #####
+
+    #####
 
     # Dump
     if dump:
